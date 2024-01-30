ad45cbfd1675b4f766c35973a9e7e95da68d4130
bajew <bajew.dmitri@web.de>
2024-01-30 19:52:57 +0100

    Simplte Webassembly Server Prerender Example

--- BlazorWasmWorkerHubExample/Server/Program.cs
+++ BlazorWasmWorkerHubExample/Server/Program.cs
@@ -1,4 +1,7 @@
+using BlazorWasmWorkerHubExample.Client.DragAndDrop;
 using BlazorWasmWorkerHubExample.Server;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.ResponseCompression;
 
@@ -13,8 +16,18 @@ builder.Services.AddResponseCompression(opts =>
 {
     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[] { "application/octet-stream" });
 });
-builder.Services.AddSingleton<Worker>(); 
+builder.Services.AddSingleton<Worker>();
 builder.Services.AddHostedService<Worker>(p => p.GetRequiredService<Worker>());
+
+
+builder.Services.AddScoped(sp =>
+{
+    var server = sp.GetRequiredService<IServer>();
+    var addressFeature = server.Features.Get<IServerAddressesFeature>();
+    string? baseAddress = addressFeature?.Addresses?.FirstOrDefault();
+    return new HttpClient { BaseAddress = new Uri(baseAddress!) };
+});
+builder.Services.AddSingleton<DragAndDropService>();
 
 var app = builder.Build();
 
@@ -42,6 +55,6 @@ app.UseRouting();
 app.MapHub<ChatHub>("/hubs/chathub");
 app.MapRazorPages();
 app.MapControllers();
-app.MapFallbackToFile("index.html");
+app.MapFallbackToPage("/_Host");
 
 app.Run();
